121f469528dd7b3a02c0f711004530df763b9cc6
jikar <julienjandot@orange.fr>
2015-12-03 23:47:07 +0100

    Mobs - UPDATE

--- src/iut/tower_defence/data/character/tower/Tower.java
+++ src/iut/tower_defence/data/character/tower/Tower.java
@@ -6,7 +6,10 @@ import iut.tower_defence.data.character.Character;
  * Created by Romain on 03/12/2015.
  */
 public class Tower extends Character {
-    public Tower () { super(50, 3, 10, 1, 12, false); } //default
+
+    public Tower () {
+        super(50, 3, 10, 1, 12, false);
+    } //default
 
 
 }
