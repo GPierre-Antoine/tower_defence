f9ea312d92634c06b22fcc0afcd1eb0032989e7a
jikar <julienjandot@orange.fr>
2015-12-03 22:25:35 +0100

    Mobs - UPDATE

---
+++ src/iut/tower_defence/data/character/monster/Archer.java
@@ -0,0 +1,13 @@
+package iut.tower_defence.data.character.monster;
+
+/**
+ * Created by julien on 03/12/2015.
+ */
+public class Archer extends MMelee{
+
+    public Archer(int m_Life, int m_Defense, int m_Attack, int m_Speed, int m_Range, int m_SpeedAttack, boolean m_isDead) {
+        super(m_Life, m_Defense, m_Attack, m_Speed, m_Range, m_SpeedAttack, m_isDead);
+    }
+
+
+}
--- src/iut/tower_defence/data/character/monster/MMelee.java
+++ src/iut/tower_defence/data/character/monster/MMelee.java
@@ -4,4 +4,8 @@ package iut.tower_defence.data.character.monster;
  * Created by pierre-antoine on 03/12/15.
  */
 public class MMelee extends Monster {
+
+    public MMelee(int m_Life, int m_Defense, int m_Attack, int m_Speed, int m_Range , int m_SpeedAttack, boolean m_isDead) {
+        super(m_Life, m_Defense, m_Attack, m_Speed, 1, m_SpeedAttack, m_isDead);
+    } //MMelee
 }
