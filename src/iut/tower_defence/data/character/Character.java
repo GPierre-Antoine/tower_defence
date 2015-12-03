458135ec03469e654c97028c588fec818c1d9bb3
Romain <hollyg509@gmail.com>
2015-12-03 22:57:45 +0100

    Character - Modify Monster - Modify

--- src/iut/tower_defence/data/character/Character.java
+++ src/iut/tower_defence/data/character/Character.java
@@ -6,4 +6,20 @@ import iut.tower_defence.data.GameObject;
  * Created by pierre-antoine on 03/12/15.
  */
 public class Character extends GameObject {
+
+    private int life;
+    private int defense;
+    private int attack;
+    private int speedAttack;
+    private int range;
+    private boolean isDead;
+
+    public Character(int life, int defense, int attack, int speed, int speedAttack, int range, boolean isDead) {
+        this.life = life;
+        this.defense = defense;
+        this.attack = attack;
+        this.speedAttack = speedAttack;
+        this.range = range;
+        this.isDead = isDead;
+    }
 }
