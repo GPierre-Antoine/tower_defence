c3c4d5c931400908670694bcd84d7dfefd1d8d8c
darkicex3 <maxime.junique@gmail.com>
2015-12-03 23:32:28 +0100

    Mobs - Update

--- src/iut/tower_defence/data/character/monster/MRange.java
+++ src/iut/tower_defence/data/character/monster/MRange.java
@@ -5,7 +5,7 @@ package iut.tower_defence.data.character.monster;
  */
 public class MRange extends Monster {
 
-    public MRange(int life, int defense, int attack, int speed, int speedAttack, int range, boolean isDead) {
-        super(life, defense, attack, speed, speedAttack, range, isDead);
-    }
+    public MRange() {
+        super(20, 5, 10, 1, 3, 10, false);
+    } //Default
 }
