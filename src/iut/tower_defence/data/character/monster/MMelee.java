842446e54df3a3bd0ba2b2575dcc3de456737318
darkicex3 <maxime.junique@gmail.com>
2015-12-03 22:45:32 +0100

    Mobs - Update

--- src/iut/tower_defence/data/character/monster/MMelee.java
+++ src/iut/tower_defence/data/character/monster/MMelee.java
@@ -5,7 +5,7 @@ package iut.tower_defence.data.character.monster;
  */
 public class MMelee extends Monster {
 
-    public MMelee(int m_Life, int m_Defense, int m_Attack, int m_Speed, int m_Range , int m_SpeedAttack, boolean m_isDead) {
-        super(m_Life, m_Defense, m_Attack, m_Speed, 1, m_SpeedAttack, m_isDead);
-    } //MMelee
+    public MMelee(int life, int defense, int attack, int speed, int speedAttack, int range, boolean isDead) {
+        super(life, defense, attack, speed, speedAttack, 1, isDead);
+    }
 }
--- src/iut/tower_defence/data/character/monster/Monster.java
+++ src/iut/tower_defence/data/character/monster/Monster.java
@@ -6,21 +6,21 @@ import iut.tower_defence.data.character.Character;
  * Created by pierre-antoine on 03/12/15.
  */
 public class Monster extends Character {
-    private int m_Life;
-    private int m_Defense;
-    private int m_Attack;
-    private int m_Speed;
-    private int m_SpeedAttack;
-    private int m_Range;
-    private boolean m_isDead;
+    private int Life;
+    private int Defense;
+    private int Attack;
+    private int Speed;
+    private int SpeedAttack;
+    private int Range;
+    private boolean isDead;
 
-    public Monster(int m_Life, int m_Defense, int m_Attack, int m_Speed, int m_Range, int m_SpeedAttack, boolean m_isDead) {
-        this.m_Life = m_Life;
-        this.m_Defense = m_Defense;
-        this.m_Attack = m_Attack;
-        this.m_Speed = m_Speed;
-        this.m_Range = m_Range;
-        this.m_SpeedAttack = m_SpeedAttack;
-        this.m_isDead = m_isDead;
-    } //Monster
+    public Monster(int life, int defense, int attack, int speed, int speedAttack, int range, boolean isDead) {
+        Life = life;
+        Defense = defense;
+        Attack = attack;
+        Speed = speed;
+        SpeedAttack = speedAttack;
+        Range = range;
+        this.isDead = isDead;
+    }
 }
