780122565d905732458b28107e95fa1b276144a8
Romain <hollyg509@gmail.com>
2015-12-03 23:40:59 +0100

    Tower - Upload

---
+++ src/iut/tower_defence/data/character/tower/Tower.java
@@ -0,0 +1,12 @@
+package iut.tower_defence.data.character.tower;
+
+import iut.tower_defence.data.character.Character;
+
+/**
+ * Created by Romain on 03/12/2015.
+ */
+public class Tower extends Character {
+    public Tower () { super(50, 3, 10, 1, 12, false); } //default
+
+
+}
