fb5ff613838e91429b983b249aa45f963c704002
Kevin HOARAU <Kevin HOARAU>
2015-12-03 23:44:06 +0100

    PLAYER UP

--- src/iut/tower_defence/data/character/Player.java
+++ src/iut/tower_defence/data/character/Player.java
@@ -4,4 +4,11 @@ package iut.tower_defence.data.character;
  * Created by pierre-antoine on 03/12/15.
  */
 public class Player extends Character {
+
+    private String name;
+
+    public Player(String name) {
+        super();
+        this.name = name;
+    } // Player
 }
