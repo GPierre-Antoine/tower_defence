894475fd2a83977e360aa4bab4a8de4b9967623a
Romain <hollyg509@gmail.com>
2015-12-04 00:06:32 +0100

    Player - Modify

--- src/iut/tower_defence/data/character/Player.java
+++ src/iut/tower_defence/data/character/Player.java
@@ -6,9 +6,14 @@ package iut.tower_defence.data.character;
 public class Player extends Character {
 
     private String name;
+    private int score;
 
     public Player(String name) {
         super(150, 5, 10, 1, 1, false);
         this.name = name;
+        this.score = score;
     } // Player
+
+    public void updateScore (int i) { score += i; } // updateScore
+
 }
