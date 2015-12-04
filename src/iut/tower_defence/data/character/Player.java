7e674958ab9ce09aae84c0d44397eb00094d8d12
Scyar <enzo_alex@hotmail.fr>
2015-12-04 01:21:14 +0100

    Player - modified

--- src/iut/tower_defence/data/character/Player.java
+++ src/iut/tower_defence/data/character/Player.java
@@ -10,12 +10,15 @@ public class Player extends Character {
     private String name;
     private int score;
 
-    public Player(String name) {
+    public Player() {
         super(150, 5, 10, 1, 1, false);
-        this.name = name;
         this.score = score;
     } // Player
 
     public void updateScore (int i) { score += i; } // updateScore
+
+    public void setName (String n) {
+        this.name = n;
+    }
 
 }
