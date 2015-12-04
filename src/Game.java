bb6f5a330ebb4ac9721d0fc95a50d9c241012aaf
Alexandre <alex83690@live.fr>
2015-12-04 02:40:47 +0100

    Ajout des fenetres et modification Main

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,12 @@
 import iut.tower_defence.data.GameObject;
+import iut.tower_defence.data.character.Player;
+import iut.tower_defence.data.character.monster.MMelee;
+import iut.tower_defence.data.character.monster.MRange;
+import iut.tower_defence.directives.Control;
 import iut.tower_defence.ressource.RSManager;
 import iut.tower_defence.ressource.factory.GameObjectFactory;
+import iut.tower_defence.ressource.factory.MMeleeFactory;
+import iut.tower_defence.ressource.factory.PlayerFactory;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
@@ -16,6 +22,7 @@ public class Game extends BasicGameState
     public static int ID;
     private static String URL_GAME = "iut/tower_defence/image/game.jpg";
     private Image gameImage;
+    private GameObject gameObject;
 
     public Game(int ID) {
         Game.ID = ID;
@@ -28,15 +35,19 @@ public class Game extends BasicGameState
 
     public void init(GameContainer container, StateBasedGame game) throws SlickException {
         gameImage = new Image(URL_GAME);
+        //gameObject = new GameObject();
     }
 
     public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
         int pos = (gameImage.getWidth() / 2 - WindowGame.GAME / 2);
         //gameImage.draw((WindowGame.WIDTH / 2) - (gameImage.getWidth() / 2), 0, 800, WindowGame.HEIGHT);
-        gameImage.drawEmbedded(0, 0, gameImage.getWidth(), WindowGame.HEIGHT, 0, 0, WindowGame.WIDTH, WindowGame.HEIGHT);
+        //gameImage.setCenterOfRotation(gameImage.getWidth() / 2, gameImage.getHeight() / 2);
+        gameImage.drawEmbedded(-gameImage.getWidth() / 2, 0, gameImage.getWidth() / 2, WindowGame.HEIGHT, -WindowGame.WIDTH / 2, 0, WindowGame.WIDTH / 2, WindowGame.HEIGHT);
+        //gameImage.draw(WindowGame.WIDTH / 2 - gameImage.getWidth() / 2, 0, 3);
         //splashscreen.draw(0, 0, WindowGame.WIDTH, WindowGame.HEIGHT);
     }
     public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
         RSManager.getInstance().update();
+        RSManager.getInstance().render();
     }
 }
